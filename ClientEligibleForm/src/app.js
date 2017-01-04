159634ce3176a0c2ab95ce175c0a0286bc8364b7
STONEGATEMTG\ecaldwell <elvin.caldwell@stonegatemtg.com>
2017-01-04 17:07:26 -0500

    updated Code for Eligibility

--- ClientEligibleForm/src/app.js
+++ ClientEligibleForm/src/app.js
@@ -7,12 +7,14 @@
 
     // create the list of sushi rolls 
     $scope.sushi = [
-      { name: 'Carl Baynard', address: '4358 Burnside Avenue', phone: '317-287-8484', loan_numb: 1900892 },
-      { name: 'Brian A Smith', address: 'Po Box 552, Plains', phone: '317-555-3289', loan_numb: 1000490 },
-      { name: 'Harry L Johnson', address: '66 Gay St, Plain City, OH', phone: '317-905-3889', loan_numb: 20078007 },
-      { name: 'Cleve H Johnson', address: '374 Wilson Ave, Janesville', phone: '317-765-3189', loan_numb: 10050006 }
+      { name: "Carl Baynard", address: "4358 Burnside Avenue", phone: "317-287-8484", loan_numb: "1900892", eligible: "Eligible" },
+      { name: "Brian A Smith", address: "Po Box 552, Plains", phone: "317-555-3289", loan_numb: "1000490", eligible: "Non Eligible" },
+      { name: "Harry L Johnson", address: "66 Gay St, Plain City, OH", phone: "317-905-3889", loan_numb: "20078007", eligible: "Non Eligible" },
+      { name: "Cleve H Johnson", address: "374 Wilson Ave, Janesville", phone: "317-765-3189", loan_numb: "10050006", eligible: "Eligible" }
     ];
 
+
+    //$scope.search = 'Search Client';
     $scope.IsVisible = false;
     $scope.ShowHide = function() {
         //if Div is visible it will be hidden and vice vesa.
